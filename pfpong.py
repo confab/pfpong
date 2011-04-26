a124ff680a0665ee0d22f58a6f86e82e7a503c00
confab <blooough@gmail.com>
2011-04-25 22:16:23 -0700

    added some funky collision detection

--- pfpong.py
+++ pfpong.py
@@ -4,7 +4,9 @@
 import sys
 import pygame
 
+#width = 1366
 width = 640
+#height = 768
 height = 480
 size = width, height
 speed = [1, 1]
@@ -16,36 +18,44 @@ ball = ball_image.get_rect()
 paddle_image = pygame.image.load('paddle.gif')
 left = paddle_image.get_rect()
 right = paddle_image.get_rect()
+left.inflate(-2, -2)
+right.inflate(-2, -2)
 left_start = [40, height / 2 - 50]
 right_start = [width - 60, height / 2 - 50]
 left = left.move(left_start)
 right = right.move(right_start)
 left_pos = [0, 0]
 right_pos = [0, 0]
+ball_hit = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                left_pos[1] = -10
-                left = left.move(left_pos)
-            elif event.key == pygame.K_s:
-                left_pos[1] = 10
-                left = left.move(left_pos)
-            elif event.key == pygame.K_UP:
-                right_pos[1] = -10
-                right = right.move(right_pos)
-            elif event.key == pygame.K_DOWN:
-                right_pos[1] = 10
-                right = right.move(right_pos)
+    if pygame.key.get_pressed()[pygame.K_w] and left.top > 0:
+        left_pos[1] = -2
+        left = left.move(left_pos)
+    elif pygame.key.get_pressed()[pygame.K_s] and left.bottom < height:
+        left_pos[1] = 2
+        left = left.move(left_pos)
+    elif pygame.key.get_pressed()[pygame.K_UP] and right.top > 0:
+        right_pos[1] = -2
+        right = right.move(right_pos)
+    elif pygame.key.get_pressed()[pygame.K_DOWN] and right.bottom < height:
+        right_pos[1] = 2
+        right = right.move(right_pos)
 
     ball = ball.move(speed)
     if ball.left < 0 or ball.right > width:
         speed[0] = -speed[0]
     if ball.top < 0 or ball.bottom > height:
         speed[1] = -speed[1]
+
+    if ball.colliderect(left) or ball.colliderect(right) and not ball_hit:
+        speed[0] = -speed[0]
+        ball_hit = not ball_hit
+    elif ball_hit:
+        ball_hit = not ball_hit
 
     screen.fill(black)
     screen.blit(ball_image, ball)
